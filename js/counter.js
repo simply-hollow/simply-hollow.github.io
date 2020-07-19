1e09c6ad33ba1c43eb254aefcec05539e88005bf
simply-hollow <akp998rahul@gmail.com>
2020-07-19 12:27:32 +0530

    minor changes

--- js/counter.js
+++ js/counter.js
@@ -1,8 +1,8 @@
 $('.count-number').each(function () {
   var $this = $(this);
   jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-    duration: 3000,
-    easing: 'easeInOutQuart',
+    duration: 2500,
+    easing: 'swing',
     step: function () {
       $this.text(Math.ceil(this.Counter));
     }
